260b0bde6dd79b064acefa160381b435f4ef6c2d
Makoto Hamanaka <ham.lua@gmail.com>
2025-03-29 23:50:55 +0900

    fix type error of csbindgen-generated binding file

--- buildrs/csbindgen_generator.rs
+++ buildrs/csbindgen_generator.rs
@@ -3,28 +3,15 @@ use std::{
     path::Path,
 };
 
-// find all .rs files in a directory recursively
-fn find_rust_files_recursive(dir: &Path, files: &mut Vec<String>) {
-    if let Ok(entries) = fs::read_dir(dir) {
-        for entry in entries.filter_map(Result::ok) {
-            let path = entry.path();
-            if path.is_dir() {
-                find_rust_files_recursive(&path, files);
-            } else if let Some(extension) = path.extension() {
-                if extension == "rs" {
-                    if let Some(path_str) = path.to_str() {
-                        files.push(path_str.to_string());
-                    }
-                }
-            }
-        }
-    }
-}
+// 定数としてターゲットファイルパスを定義
+const TARGET_FILE_PATH: &str = "target/ZenohNative.g.cs";
 
 pub fn generate_csharp_binding() {
     let mut rust_files = Vec::new();
-    let src_dir = Path::new("src");
-    find_rust_files_recursive(src_dir, &mut rust_files);
+    find_rust_files_recursive(Path::new("src"), &mut rust_files);
+    find_rust_files_recursive(Path::new("src/opaque_types"), &mut rust_files);
+    find_rust_files_recursive(Path::new("src/closures"), &mut rust_files);
+    find_rust_files_recursive(Path::new("src/platform"), &mut rust_files);
 
     let mut builder = csbindgen::Builder::default();
     // input files other than result.rs
@@ -39,11 +26,43 @@ pub fn generate_csharp_binding() {
         .csharp_class_name("ZenohNative")
         .csharp_namespace("Zenoh.Plugins")
         .csharp_dll_name_if("UNITY_IOS && !UNITY_EDITOR", "__Internal")
+        .csharp_use_function_pointer(false)
         .csharp_generate_const_filter(|_|true)
-        .csharp_type_rename(|rust_type_name| {
-            println!("cargo:warning=type: {}", rust_type_name);
-            return rust_type_name;
-        })
-        .generate_csharp_file("target/ZenohNative.g.cs")
+        .generate_csharp_file(TARGET_FILE_PATH)
         .unwrap();
+    
+    replace_enum_type_in_generated_file();
+}
+
+// find all .rs files in a directory recursively
+fn find_rust_files_recursive(dir: &Path, files: &mut Vec<String>) {
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries.filter_map(Result::ok) {
+            let path = entry.path();
+            if path.is_dir() {
+                // don't process child folders
+            } else if let Some(extension) = path.extension() {
+                if extension == "rs" {
+                    if let Some(path_str) = path.to_str() {
+                        files.push(path_str.to_string());
+                    }
+                }
+            }
+        }
+    }
+}
+
+// fix z_consolidation_mode_t error, where uint type is generated but it has -1 value.
+pub fn replace_enum_type_in_generated_file() {
+    if let Ok(content) = fs::read_to_string(TARGET_FILE_PATH) {
+        let updated_content = content.replace(
+            "enum z_consolidation_mode_t : uint",
+            "enum z_consolidation_mode_t : int",
+        );
+        if let Err(e) = fs::write(TARGET_FILE_PATH, updated_content) {
+            eprintln!("Failed to write updated file: {}", e);
+        }
+    } else {
+        eprintln!("Failed to read the file: {}", TARGET_FILE_PATH);
+    }
 }
